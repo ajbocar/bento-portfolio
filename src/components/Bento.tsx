0fbd6460d32988450621809b930bda03c5950ce1
ajbocar <ajbocar@gmail.com>
2024-05-13 09:21:24 +0800

    Update Bento.tsx
    feat: add logo, footer and animation

--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -5,8 +5,16 @@ import { SiGithub, SiLinkedin, SiCodepen, SiX } from "react-icons/si";
 
 export const Bento = () => {
   return (
-    <div className="min-h-screen bg-zinc-900 px4 py12 text-zinc-50">
-      <div className="mx-auto max-w-4xl grid grid-cols-12 gap-4">
+    <div className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
+      <Logo/>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        transition={{
+          staggerChildren: 0.05,
+        }}
+        className="mx-auto max-w-4xl grid grid-cols-12 gap-4"
+      >
         <HeaderBlock />
         <SocialsBlock />
         <Block />
@@ -17,7 +25,8 @@ export const Bento = () => {
         <Block />
         <Block />
         <Block />
-      </div>
+      </motion.div>
+      <Footer/>
     </div>
   );
 };
@@ -29,6 +38,24 @@ type Props = {
 const Block = ({ className, ...rest }: Props) => {
   return (
     <motion.div
+      variants={{
+        initial: {
+          scale: 0.5,
+          y: 50,
+          opacity: 0,
+        },
+        animate: {
+          scale: 1,
+          y: 0,
+          opacity: 1,
+        },
+      }}
+      transition={{
+        type: "spring",
+        mass: 3,
+        stiffness: 400,
+        damping: 50,
+      }}
       className={twMerge(
         "col-span-4 rounded-lg border border-zinc-700 bg-zinc-800 p-6",
         className
@@ -39,10 +66,11 @@ const Block = ({ className, ...rest }: Props) => {
 };
 
 const HeaderBlock = () => {
+  //dicebear.com
   return (
     <Block className="col-span-12 row-span-2 md:col-span-6">
       <img
-        src="https://api.dicebear.com/8.x/big-smile/svg?seed=Bailey"
+        src="https://api.dicebear.com/8.x/avataaars-neutral/svg?seed=Jack"
         alt="avatar"
         className="mb-4 size-14 rounded-full"
       />
@@ -64,12 +92,13 @@ const HeaderBlock = () => {
 const SocialsBlock = () => {
   return (
     <>
-      <Block 
-      whileHover={{
-        scale: 1.05,
-        rotate: 2,
-      }}
-      className="col-span-6 md:col-span-3 bg-green-500">
+      <Block
+        whileHover={{
+          scale: 1.05,
+          rotate: 2,
+        }}
+        className="col-span-6 md:col-span-3 bg-green-500"
+      >
         <a
           href="#"
           className="grid h-full place-content-center text-3xl text-white"
@@ -102,5 +131,41 @@ const SocialsBlock = () => {
         </a>
       </Block>
     </>
+  );
+};
+const Logo = () => {
+  //logoipsum.com
+  return (
+    <svg
+      id="logo-70"
+      width="40"
+      height="auto"
+      viewBox="0 0 78 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="mx-auto mb-12 fill-zinc-50"
+    >
+      {" "}
+      <path
+        d="M18.5147 0C15.4686 0 12.5473 1.21005 10.3934 3.36396L3.36396 10.3934C1.21005 12.5473 0 15.4686 0 18.5147C0 24.8579 5.14214 30 11.4853 30C14.5314 30 17.4527 28.7899 19.6066 26.636L24.4689 21.7737C24.469 21.7738 24.4689 21.7736 24.4689 21.7737L38.636 7.6066C39.6647 6.57791 41.0599 6 42.5147 6C44.9503 6 47.0152 7.58741 47.7311 9.78407L52.2022 5.31296C50.1625 2.11834 46.586 0 42.5147 0C39.4686 0 36.5473 1.21005 34.3934 3.36396L15.364 22.3934C14.3353 23.4221 12.9401 24 11.4853 24C8.45584 24 6 21.5442 6 18.5147C6 17.0599 6.57791 15.6647 7.6066 14.636L14.636 7.6066C15.6647 6.57791 17.0599 6 18.5147 6C20.9504 6 23.0152 7.58748 23.7311 9.78421L28.2023 5.31307C26.1626 2.1184 22.5861 0 18.5147 0Z"
+        stopColor="#000000"
+      ></path>{" "}
+      <path
+        d="M39.364 22.3934C38.3353 23.4221 36.9401 24 35.4853 24C33.05 24 30.9853 22.413 30.2692 20.2167L25.7982 24.6877C27.838 27.8819 31.4143 30 35.4853 30C38.5314 30 41.4527 28.7899 43.6066 26.636L62.636 7.6066C63.6647 6.57791 65.0599 6 66.5147 6C69.5442 6 72 8.45584 72 11.4853C72 12.9401 71.4221 14.3353 70.3934 15.364L63.364 22.3934C62.3353 23.4221 60.9401 24 59.4853 24C57.0498 24 54.985 22.4127 54.269 20.2162L49.798 24.6873C51.8377 27.8818 55.4141 30 59.4853 30C62.5314 30 65.4527 28.7899 67.6066 26.636L74.636 19.6066C76.7899 17.4527 78 14.5314 78 11.4853C78 5.14214 72.8579 0 66.5147 0C63.4686 0 60.5473 1.21005 58.3934 3.36396L39.364 22.3934Z"
+        stopColor="#000000"
+      ></path>{" "}
+    </svg>
+  );
+};
+const Footer = () => {
+  return(
+    <footer className="mt-12">
+      <p className="text-center text-zinc-400">
+        Made with ❤️ by{" "}
+        <a href="#" className="text-red-300 hover:underline">
+          @ajbocar
+        </a>
+      </p>
+    </footer>
   );
 };
